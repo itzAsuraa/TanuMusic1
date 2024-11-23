2c1cb895af6758e61aff2772497e40d86d4de1be
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-11-23 21:30:18 +0530

    Update misc.py

--- TanuMusic/misc.py
+++ TanuMusic/misc.py
@@ -11,7 +11,6 @@ from .logging import LOGGER
 
 SUDOERS = filters.user()
 
-
 HAPP = None
 _boot_ = time.time()
 
@@ -39,35 +38,27 @@ XCB = [
 
 def dbb():
     global db
-    global clonedb
     db = {}
-    clonedb = {}
-    LOGGER(__name__).info(f"✦ Local Database Initialized...💛.")
+    LOGGER(__name__).info(f"✦ Local Database Initialized...💛")
 
 
 async def sudo():
     global SUDOERS
-    OWNER = config.OWNER_ID
-    if config.MONGO_DB_URI is None:
-        for user_id in OWNER:
+    SUDOERS.add(config.OWNER_ID)
+    sudoersdb = mongodb.sudoers
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    sudoers = [] if not sudoers else sudoers["sudoers"]
+    if config.OWNER_ID not in sudoers:
+        sudoers.append(config.OWNER_ID)
+        await sudoersdb.update_one(
+            {"sudo": "sudo"},
+            {"$set": {"sudoers": sudoers}},
+            upsert=True,
+        )
+    if sudoers:
+        for user_id in sudoers:
             SUDOERS.add(user_id)
-    else:
-        sudoersdb = mongodb.sudoers
-        sudoers = await sudoersdb.find_one({"sudo": "sudo"})  # Await the database call
-        sudoers = [] if not sudoers else sudoers.get("sudoers", [])
-        for user_id in OWNER:
-            SUDOERS.add(user_id)
-            if user_id not in sudoers:
-                sudoers.append(user_id)
-                await sudoersdb.update_one(
-                    {"sudo": "sudo"},
-                    {"$set": {"sudoers": sudoers}},
-                    upsert=True,
-                )
-        if sudoers:
-            for x in sudoers:
-                SUDOERS.add(x)
-    LOGGER(__name__).info(f"✦ Sudoers Loaded...❤️.")
+    LOGGER(__name__).info(f"✦ Sudoers Loaded...🧡")
 
 
 def heroku():
@@ -80,5 +71,5 @@ def heroku():
                 LOGGER(__name__).info(f"✦ Heroku App Configured...💙")
             except BaseException:
                 LOGGER(__name__).warning(
-                    f"✦ Please make sure your Heroku API Key and Your App name are configured correctly in the heroku...💚."
+                    f"✦ Please make sure your Heroku API Key and Your App name are configured correctly in the heroku...💚"
                 )
